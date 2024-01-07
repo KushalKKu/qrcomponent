f0c1dafc58e784453c98d102bee597c288688353
kushal <kushalkulkarni29@gmail.com>
2024-01-07 19:54:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 function App() {
   return (
 <>
+<main>
 <div className="container ">
 <div className="card text-center" style={{width: "18rem"}}>
   <img src={qrimg} className="card-img-top" alt="qr-img"/>
   <div className="card-body">
-    <h5 className="card-title ">Improve your front-end skills by building projects</h5>
+    <h5 className="card-title fw-bold">Improve your front-end skills by building projects</h5>
     <p className="card-text"> Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
   </div>
 </div>
 </div>
+</main>
 
 </>
   );
